94604b89cad33c9504bee42777db4a7b3b33ea3d
Stephen-Stanbridge <stanbridgestephen.jr@gmail.com>
2020-04-18 11:51:40 +0200

    Test for get_ll_received_memos function.

--- Tests/test_inbox_commands.py
+++ Tests/test_inbox_commands.py
@@ -1,5 +1,5 @@
 from Handlers.inbox_commands import does_user_have_memo_in_inbox, delete_all_memos_from_inbox, \
-    delete_memo_from_inbox_by_id, get_received_memo_by_id
+    delete_memo_from_inbox_by_id, get_received_memo_by_id, get_all_received_memos
 from Models.models import SentMemo, User
 import pytest
 from conftest import does_table_contain_all_words
@@ -41,12 +41,19 @@ def test_delete_memo_from_inbox_by_id_when_memo_belongs_to_another_user(capsys, 
     assert len(session.query(SentMemo).filter(SentMemo.memo_id == 1, SentMemo.receiver_id == 1).all()) == 1
 
 
-def test_improper_get_received_memo_by_id(second_user, user, inbox):
+def test_improper_get_received_memo_by_id(second_user, inbox):
     assert get_received_memo_by_id(second_user, inbox[0].memo_id) == "You don't have memo with this id in your inbox."
     assert get_received_memo_by_id(second_user, 10) == "You don't have memo with this id in your inbox."
 
 
-def test_get_received_memo_by_id(user, inbox):
+def test_get_received_memo_by_id(user, inbox, session):
     memo = inbox[0].memo_details
     words = [str(memo.id), memo.title, memo.content, user.username]
     assert does_table_contain_all_words(words, get_received_memo_by_id(user, memo.id))
+
+
+def test_get_all_received_memos(user, inbox):
+    memo1 = inbox[0].memo_details
+    memo2 = inbox[1].memo_details
+    words = [str(memo1.id), memo1.title, memo1.content, user.username, str(memo2.id), memo2.title, memo2.content]
+    assert does_table_contain_all_words(words, get_all_received_memos(user))
