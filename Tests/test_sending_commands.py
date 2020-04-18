ee689693eff4becbe8bae2e1c2a2a84de6f0dbc4
Stephen-Stanbridge <stanbridgestephen.jr@gmail.com>
2020-04-18 14:11:02 +0200

    Test for send_memo function.

--- Tests/test_sending_commands.py
+++ Tests/test_sending_commands.py
@@ -1,6 +1,7 @@
-from Handlers.sending_commands import make_checks
+from Handlers.sending_commands import make_checks, send_memo
 from ConfigClasses.classes import SendMemo
 import pytest
+from Models.models import SentMemo, Memo
 
 
 def test_make_checks(user, second_user, memos, inbox):
@@ -11,3 +12,19 @@ def test_make_checks(user, second_user, memos, inbox):
     assert make_checks(config) == (False, "UserError")
     config.receiver = user.id
     assert not make_checks(config)
+    config.receiver = second_user.id
+    assert make_checks(config)
+
+
+def test_send_memo(user, second_user, session):
+    config = SendMemo()
+    memo_to_send = session.query(Memo).filter(Memo.creator_id == user.id).first()
+    config.user, config.memo_id, config.receiver = user, memo_to_send.id, second_user.id
+    before = len(session.query(SentMemo).filter(SentMemo.receiver_id == second_user.id,
+                                                SentMemo.memo_id == memo_to_send.id).all())
+    memo_to_send.sent = False
+    session.commit()
+    send_memo(config)
+    assert memo_to_send.sent
+    assert len(session.query(SentMemo).filter(SentMemo.receiver_id == second_user.id,
+                                              SentMemo.memo_id == memo_to_send.id).all()) == before + 1
